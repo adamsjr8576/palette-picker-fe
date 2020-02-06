c0d5a561cf0dd1bc5c486b905a8d0e52d212f91c
John Adams <adamsjr8576@gmail.com>
2020-02-06 15:14:16 -0700

    Create Header component and use in App

---
+++ src/Containers/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import Header from '../../Components/Header/Header';
+
+
+const App = () => {
+  return (
+    <main className="App">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
