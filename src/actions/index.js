cafbdb4c73fa01b4ce0e5a3bac73fd4bec61bf60
John Adams <adamsjr8576@gmail.com>
2020-02-06 21:31:44 -0700

    Add updateLockedPalette action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
 export const addNewPalette = currentPalette => ({
   type: 'ADD_PALETTE',
   currentPalette
-})
+});
+
+export const updateLockOnPalette = hexCode => ({
+  type: 'UPDATE_LOCKED_PALETTE',
+  hexCode
+});
