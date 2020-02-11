d9c28e7035de027c3cc98ec3dedb377fcb4f0e38
John Adams <adamsjr8576@gmail.com>
2020-02-10 21:07:07 -0700

    Add mapStateToProps to projectForm to bring in colors based on currentPalette

--- src/Containers/ProjectForm/ProjectForm.js
+++ src/Containers/ProjectForm/ProjectForm.js
@@ -4,7 +4,7 @@ import { addProject } from '../../actions/index';
 import './ProjectForm.scss';
 import { postProject, getProjectById } from '../../apiCalls';
 
-export const ProjectForm = ({ addProject }) => {
+export const ProjectForm = ({ addProject, currentPalette }) => {
 
   const [ newProject, setNewProject] = useState('');
 
@@ -28,17 +28,23 @@ export const ProjectForm = ({ addProject }) => {
     });
   }
 
+  let outlineColor = '#017DF1';
+  if (currentPalette.length) {
+    outlineColor = currentPalette[0].color;
+  }
+
   return(
     <form className='project-form'>
-      <label for='make-project' className='project-name-label'>Make a new project:</label>
+      <label for='make-project' className='project-name-label'>Make A New Project:</label>
       <input
         className='input-make-project'
         aria-label='name-project-input'
         placeholder='Project Name'
-        maxLength='30'
+        maxLength='20'
         type='text'
         value={newProject}
         onChange={ e => setNewProject(e.target.value) }
+        style={{border: `3px solid ${outlineColor}`}}
       />
       <button type='button' className='save-project-btn' role='button' onClick={ e => handleSubmit(e) }>Save project</button>
     </form>
@@ -49,4 +55,8 @@ export const mapDispatchToProps = dispatch => ({
   addProject: projectName => dispatch( addProject(projectName) )
 });
 
-export default connect(null, mapDispatchToProps)(ProjectForm);
+export const mapStateToProps = state => ({
+  currentPalette: state.currentPalette
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectForm);
