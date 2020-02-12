45f5a560e88636bd558b23fec6dfe2c4644f6136
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-02-12 13:42:06 -0700

    Add App test and Heroku

--- src/Containers/App/App.test.js
+++ src/Containers/App/App.test.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
+
+describe('App', () => {
+
+  describe('App Component', () => {
+
+    it('should match the snapshot', () => {
+      wrapper = shallow(<App />)
+
+      expect(wrapper).toMatchSnapshot()
+    });
+  });
+
+});
 
 
